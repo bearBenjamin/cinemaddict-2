97f0c325d14447b9b560027f2143c4baa295bb7f
Ilia Konev <122528494+bearBenjamin@users.noreply.github.com>
2026-02-10 12:15:45 +0300

    feat: добавил блок под mock-данные

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const TITLE = ['Тайна Коко', ''];
+const ALTERNATIVE_TITLE = ['Coco'];
+const DESCRIPTION = ['Главный герой, юный Мигель, живет в мексиканской деревушке и тайно мечтает стать музыкантом, несмотря на старинный семейный запрет. В День мертвых он случайно попадает в красочный Мир предков, где встречает своих покойных родственников. Чтобы вернуться домой, мальчику нужно получить благословение и раскрыть тайну своего прапрадеда. Это трогательная история о памяти, семейных узах и верности своей мечте.', ];
+const filmData = {
+  id: 0,
+  comments: [],
+  info: {
+    title: 'A Little Pony Without The Carpet',
+    alternativeTitle: 'Laziness Who Sold Themselves',
+    totalRating: 5.3,
+    poster: 'images/posters/blue-blazes.jpg',
+    ageRating: 0,
+    director: 'Tom Ford',
+    writers: [
+      'Takeshi Kitano'
+    ],
+    actors: [
+      'Morgan Freeman'
+    ],
+    release: {
+      date: '2019-05-11T00:00:00.000Z',
+      releaseСountry: 'Finland'
+    },
+    runtime: 77,
+    genre: [
+      'Comedy'
+    ],
+    description: 'Oscar-winning film, a war drama about two young people, from the creators of timeless classic "Nu, Pogodi!" and "Alice in Wonderland", with the best fight scenes since Bruce Lee.'
+  },
+  userDetails: {
+    watchlist: false,
+    alreadyWatched: true,
+    watchingDate: '2019-04-12T16:12:32.554Z',
+    favorite: false
+  }
+};
